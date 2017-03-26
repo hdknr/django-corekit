8b68c606c5a171588695a05bdf946d36251b4fc6
hdknr <gmail@hdknr.com>
2017-03-27 00:34:42 +0900

    GroupPermissionLoader

--- lib/corekit/conf.py
+++ lib/corekit/conf.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
+from django.contrib.auth import get_permission_codename
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
 from pydoc import locate as load_class
 from .methods import CoreModel
 import yaml
 import json
 import os
+import traceback
+from logging import getLogger
+logger = getLogger()
 
 
 def load_conf(path):
@@ -61,3 +67,37 @@ def load_fixtures(path):
             model.objects.load_fixture(item)
         else:
             load_model_fixtures(model, item)
+
+
+class GroupPermissionLoader(object):
+
+    def permit_action(self, group, content_type, action):
+        try:
+            codename = get_permission_codename(
+                action, content_type.model_class()._meta)
+
+            perms = Permission.objects.filter(
+                content_type=content_type, codename=codename)
+            name = "Can {} {}".format(
+                action, content_type.model_class()._meta.verbose_name_raw)
+            if perms.exists():
+                perms.update(name=name)
+                pobj = perms.first()
+            else:
+                pobj = Permission.objects.create(
+                    content_type=content_type, codename=codename, name=name)
+
+            group.permissions.add(pobj)
+        except:
+            logger.error(u"Permission failure:{} {}.{}".format(
+                action, content_type.app_label, content_type.model))
+            logger.error(traceback.format_exc())
+
+    def load(self, conf):
+        for item in conf['groups']:
+            group, _x = Group.objects.get_or_create(name=item['group'])
+            for perm in item['permissions']:
+                content_type = ContentType.objects.get_by_natural_key(
+                    *perm['model'].split('.'))
+                for action in perm['actions']:
+                    self.permit_action(group, content_type, action)
