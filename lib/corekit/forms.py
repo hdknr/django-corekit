cc7bccb57082ebebe090a576cc09be8164385d3a
hdknr <gmail@hdknr.com>
2017-05-30 13:44:10 +0900

    FormSet with request.FILES

--- lib/corekit/forms.py
+++ lib/corekit/forms.py
@@ -35,30 +35,34 @@ class ChildrenFormSet(forms.BaseInlineFormSet):
     FORM_CLASS = None
 
     @classmethod
-    def factory(cls, data, parent, extra=1, files=None, **kwargs):
+    def factory(cls, data, parent, extra=1, files=None, construct={}, **kwargs):
         formset_class = cls.create_class(
-            parent._meta.concrete_model, extra=extra)
+            parent._meta.concrete_model, extra=extra, **construct)
         return formset_class(data, files or None, instance=parent, **kwargs)
 
     @classmethod
-    def create_class(cls, parent_class, extra=1):
+    def create_class(cls, parent_class, extra=1, **kwargs):
         # https://docs.djangoproject.com/en/1.10/ref/forms/models/#inlineformset-factory
         return forms.inlineformset_factory(
             parent_class, cls.FORM_CLASS.Meta.model, form=cls.FORM_CLASS,
-            extra=extra, formset=cls)
+            extra=extra, formset=cls, **kwargs)
 
 
 class ModelFormSet(forms.BaseModelFormSet):
     FORM_CLASS = None
 
     @classmethod
-    def create_class(cls, extra=1):
+    def create_class(cls, extra=1, **kwargs):
         # https://docs.djangoproject.com/en/1.11/ref/forms/models/#modelformset-factory
         return forms.modelformset_factory(
             cls.FORM_CLASS.Meta.model, form=cls.FORM_CLASS,
-            extra=extra, formset=cls)
+            extra=extra, formset=cls, **kwargs)
 
     @classmethod
-    def factory(cls, data, queryset, extra=1, **kwargs):
-        formset_class = cls.create_class(extra=extra)
-        return formset_class(data, queryset=queryset, **kwargs)
+    def factory(
+        cls, data, queryset=None, files=None, extra=1, construct={},
+        **kwargs
+    ):
+        formset_class = cls.create_class(extra=extra, **construct)
+        return formset_class(
+            data or None, files or None, queryset=queryset, **kwargs)
