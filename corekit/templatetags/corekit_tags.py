8e1dcddfecb29ee81f38f91ba0712953e596ccb5
hdknr <gmail@hdknr.com>
2018-07-18 11:57:47 +0900

    None objects returns null in serializers

--- corekit/templatetags/corekit_tags.py
+++ corekit/templatetags/corekit_tags.py
@@ -7,5 +7,7 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def to_json(context, obj, class_name=None):
     '''Convert an object to JSON using Serializer'''
+    if not obj:
+        return 'null'
     return serializers.to_json(
         obj, class_name=class_name, request=context.get('request', None))
