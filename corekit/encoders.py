a08489319e8342e892f4fb9ca161b5f349fdc755
hdknr <gmail@hdknr.com>
2018-08-02 13:59:07 +0900

    to_yaml: OrderedDict

--- corekit/encoders.py
+++ corekit/encoders.py
@@ -2,12 +2,33 @@ from django.forms.models import model_to_dict
 from django.db.models import Model
 from django.db.models.fields.files import FieldFile
 from datetime import datetime
+from collections import OrderedDict
 from enum import Enum
 from decimal import Decimal
 from rest_framework.utils import encoders
 from io import IOBase
 import json
 import yaml
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+from yaml.representer import SafeRepresenter
+
+
+Dumper.add_representer(
+    OrderedDict, 
+    lambda dumper, data:dumper.represent_dict(data.items())
+)
+
+Dumper.add_representer(
+    str, SafeRepresenter.represent_str)
+
+Loader.add_constructor(
+    yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+    lambda loader, node: OrderedDict(loader.construct_pairs(node))
+)
+
 
 IGNORES = (IOBase, )
 
@@ -34,9 +55,11 @@ class JSONEncoder(encoders.JSONEncoder):
 
     @classmethod
     def to_yaml(cls, obj, *args, **kwargs):
+        if not 'Dumper' in kwargs:
+            kwargs['Dumper'] = Dumper
         if isinstance(obj, Model):
             obj = model_to_dict(obj)
-        return yaml.safe_dump(obj, *args, **kwargs)
+        return yaml.dump(obj, *args, **kwargs)
 
 to_json = JSONEncoder.to_json
 from_json = JSONEncoder.from_json
