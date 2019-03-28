cd68f872d540612c736c9853adee7276e0032f59
hdknr <gmail@hdknr.com>
2019-03-28 14:12:03 +0900

    dup_instance utility

--- corekit/models.py
+++ corekit/models.py
@@ -1,5 +1,8 @@
-# coding: utf-8
-from __future__ import unicode_literals
-
-# from django.db import models
-# Create your models here.
+def dup_instance(instance, updates={}, exclude=['id', 'pk']):
+    '''duplicate instance'''
+    params = dict(
+        (f.name, getattr(instance, f.name, None))
+        for f in instance._meta.fields
+        if f.name not in exclude)
+    params.update(updates)
+    return instance._meta.model.objects.create(**params)
